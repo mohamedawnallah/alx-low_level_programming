9b22109b6517b233d3e4cd61b338c65f33cc56db
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-02-22 06:14:09 +0200

    Add main function documentation

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - entrypoint
+ * Description: prints _putchar, followed by a new line.
+ * Returns: Always 0 (Success) 
+*/
+
 int main(void)
 {
 	printf("_putchar\n");
