70a75287bdcf599dc93f2a1821f22249a06fe8b4
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-02-22 06:45:52 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,7 +10,7 @@ void print_alphabet(void)
 {
     char character;
 
-    for (character = 'a'; character = 'z'; character++)
+    for (character = 'a'; character <= 'z'; character++)
     {
     	_putchar(character);
     }
