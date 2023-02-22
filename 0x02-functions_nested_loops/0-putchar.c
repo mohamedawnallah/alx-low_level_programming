b2e65dcbe466f65e2edf65cfe6e2a2c46da3c9ab
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-02-22 06:20:13 +0200

    Remove trailing whitespace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,11 @@
 /**
  * main - entrypoint
  * Description: prints _putchar, followed by a new line.
- * Returns: Always 0 (Success) 
+ * Returns: Always 0 (Success)
 */
 
 int main(void)
 {
 	printf("_putchar\n");
 	return (0);
-} 
+}
