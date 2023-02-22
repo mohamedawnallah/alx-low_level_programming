6f07883646bb56c3eca64c5c06e3e0742c88d0aa
mhmdawnallah <mohamedmohey2352@gmail.com>
2023-02-22 03:38:21 +0200

    Refine 0x00-hello_world code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,8 @@ int main(void)
 
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a signed int: %lu byte(s)\n", sizeof(int));
+	printf("Size of an unsigned int: %lu byte(s)\n", sizeof(unsigned int));
 	printf("Size of an double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
